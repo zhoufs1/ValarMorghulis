ea12704720d636b0b33cebd7534c5b5a1f5fbad5
zhoufs1 <zhoufs1@yonyou.com>
2018-05-17 15:49:21 +0800

    修改配置

---
+++ src/components/menu/MenuPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{msg}}}</div>
+  <div>{{hmz}}</div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App',
+        hmz:"SB"
+      }
+    }
+  }
+</script>
