985299bd5b3e11d06f0c74d631c363796d4cf66a
zhoufs1 <zhoufs1@yonyou.com>
2018-05-17 16:48:23 +0800

    修改配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import MenuPage from '@/components/menu/MenuPage'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    },
+    {
+      path: '/menu',
+      name: 'MenuPage',
+      component: MenuPage
+    },
   ]
 })
